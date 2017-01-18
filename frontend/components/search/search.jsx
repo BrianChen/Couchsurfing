fb1c95c5c7fa03d617385560de377f4c54528ca6
brian <brianc.cb@gmail.com>
2017-01-18 13:07:40 -0800

    Fixed search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,37 +4,12 @@ import ListingIndexContainer from './listing_index_container';
 import FilterForm from './filter_form';
 
 class Search extends React.Component {
+  
   constructor(props){
     super(props);
-    this.state = {
-      listings: this.props.listings,
-      updateFilter: this.props.updateFilter,
-      startDate: this.props.startDate,
-      endDate: this.props.endDate,
-      center: this.props.center
-    }
-  }
-
-  componentWillReceiveProps(){
-    console.log("Inside will receive props");
-    console.log(this.props.listings);
-  }
-
-  componentWillMount(){
-    console.log("Inside will mount");
-    console.log(this.props.listings);
-    this.setState({
-      listings: this.props.listings,
-      updateFilter: this.props.updateFilter,
-      startDate: this.props.startDate,
-      endDate: this.props.endDate,
-      center: this.props.center
-    })
   }
 
   render() {
-    console.log("Inside render");
-    console.log(this.props.listings);
     return (
       <div className="search-page">
         <div className="left-half">
