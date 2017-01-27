c803e6d325cc57f452285dfb43e3a37812826372
brian <brianc.cb@gmail.com>
2017-01-26 17:22:19 -0800

    Added to splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -56,6 +56,43 @@ class Splash extends React.Component {
               </ul>
             </div>
           </section>
+
+          <section className="splash2">
+            <div className="splash2-left">
+              <h2>Explore the World</h2>
+              <button>How It Works</button>
+            </div>
+            <div className="splash2-right">
+              <article>
+                <h3>Connected With</h3>
+                <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1485392793/airplane-11-32_kjgdtg.png"></img>
+                <div className="testimonial-images">
+                  <figure>
+                    <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1485393547/person1_ibiaim.jpg"></img>
+                    <figcaption>Alexa</figcaption>
+                  </figure>
+                  <figure>
+                    <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1485394632/person2_ywj6xi.png"></img>
+                    <figcaption>Jenny</figcaption>
+                  </figure>
+                </div>
+                <p className="testimonial-subtitle">in New York, New York</p>
+                <div className="border"></div>
+                <p className="testimonial-text">
+                  When I was traveling solo around United States this summer, I used Floorsurfing to meet new people. Jenny was at the weekly Floorsurfing event in New York and I feel so lucky to have met him! I enjoyed our conversations about life, and she showed me the best shop to grab an authentic coffee. After just a few short days of hanging in the city, I felt as though I had known her for years.
+                </p>
+              </article>
+            </div>
+          </section>
+
+          <section className="splash-footer">
+            <ul>
+              <li>About</li>
+              <li>How It Works</li>
+              <li>Safety</li>
+            </ul>
+            <p>© 2017 Brian Chen</p>
+          </section>
         </div>
       </main>
     );
