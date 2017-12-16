1a696b7460ba8b7021aabca4d644a809809750be
brian <brianc.cb@gmail.com>
2017-12-15 22:14:09 -0500

    Implemented responsive splash page

--- frontend/components/Pages/home.jsx
+++ frontend/components/Pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'react-modal';
 import { Link } from 'react-router';
+import PropTypes from 'prop-types'
 
 import SignUpFormContainer from '../Forms/signup_form_container';
 import LoginFormContainer from '../Forms/login_form_container';
@@ -28,7 +29,7 @@ class Home extends React.Component {
   }
 
   onModalClose() {
-    this.setState({modalOpen: false})
+    this.setState({modalOpen: false});
   }
 
   modal() {
@@ -54,8 +55,7 @@ class Home extends React.Component {
               <h1>Stay with Locals and Meet Travelers</h1>
               <h2>Share Authentic Travel Experiences</h2>
               <ul>
-                <li><button className="splash1-join-button" onClick={this.handleClick.bind(this, true)}>Join With Email</button></li>
-                {this.modal()}
+                <li><button className="splash1-join-button" onClick={this.props.guestLogin}>Login as Guest</button></li>
               </ul>
             </div>
           </section>
@@ -65,50 +65,55 @@ class Home extends React.Component {
               <h2>Explore the World</h2>
               <button className="blue-btn"><Link to='/howitworks'>How It Works</Link></button>
             </div>
-            <div className="splash2-right">
-              <article>
-                <h3>Connected With</h3>
-                <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345059/airplane-icon_hamyue.png"></img>
-                <div className="testimonial-images">
-                  <figure>
-                    <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/r_0/v1512345282/person1_ph6btg.jpg"></img>
-                    <figcaption>Alexa</figcaption>
-                  </figure>
-                  <figure>
-                    <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345559/person2_jwwcs3.png"></img>
-                    <figcaption>Jenny</figcaption>
-                  </figure>
-                </div>
-                <p className="testimonial-subtitle">in New York, New York</p>
-                <div className="border"></div>
-                <p className="testimonial-text">
-                  When I was traveling solo around United States this summer, I used Floorsurfing to meet new people. Jenny was at the weekly Floorsurfing event in New York and I feel so lucky to have met her! I enjoyed our conversations about life, and she showed me the best shop to grab an authentic coffee. After just a few short days of hanging in the city, I felt as though I had known her for years.
-                </p>
-              </article>
+            <div className="splash2-top">
+              <h2>Explore the World</h2>
+              <button className="blue-btn"><Link to='/howitworks'>How It Works</Link></button>
+            </div>
+            <div className="splash-article">
+              <h3>Connected With</h3>
+              <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345059/airplane-icon_hamyue.png"></img>
+              <div className="testimonial-images">
+                <figure>
+                  <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/r_0/v1512345282/person1_ph6btg.jpg"></img>
+                  <figcaption>Alexa</figcaption>
+                </figure>
+                <figure>
+                  <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345559/person2_jwwcs3.png"></img>
+                  <figcaption>Jenny</figcaption>
+                </figure>
+              </div>
+              <p className="testimonial-subtitle">in New York, New York</p>
+              <div className="border"></div>
+              <p className="testimonial-text">
+                When I was traveling solo around United States this summer, I used Floorsurfing to meet new people. Jenny was at the weekly Floorsurfing event in New York and I feel so lucky to have met her! I enjoyed our conversations about life, and she showed me the best shop to grab an authentic coffee. After just a few short days of hanging in the city, I felt as though I had known her for years.
+              </p>
             </div>
           </section>
 
-          <section className="splash2">
-            <div className="splash2-right">
-              <article>
-                <h3>Stayed With</h3>
-                <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345059/airplane-icon_hamyue.png"></img>
-                <div className="testimonial-images">
-                  <figure>
-                    <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345806/person3_w0czpm.jpg"></img>
-                    <figcaption>Jeff</figcaption>
-                  </figure>
-                  <figure>
-                    <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512346149/person4_eb7zgd.jpg"></img>
-                    <figcaption>Anna</figcaption>
-                  </figure>
-                </div>
-                <p className="testimonial-subtitle">in San Francisco, California</p>
-                <div className="border"></div>
-                <p className="testimonial-text">
-                  Anna is amazing! I loved spending time with her, and her openness and caring nature was so genuine and refreshing. When my suitcase didn't arrive until my second day, she was kind enough to loan me the comfiest pajamas to wear to sleep that first night, thank you again! A highlight of my trip includes celebrating her birthday with a dance party in the living room. Hope to see you again soon!
-                </p>
-              </article>
+          <section className="splash3">
+            <div className="splash3-top">
+              <h2>Meet New People</h2>
+              <button className="orange-btn" onClick={this.handleClick.bind(this, true)}>Plan a Trip</button>
+              {this.modal()}
+            </div>
+            <div className="splash-article">
+              <h3>Stayed With</h3>
+              <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345059/airplane-icon_hamyue.png"></img>
+              <div className="testimonial-images">
+                <figure>
+                  <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512345806/person3_w0czpm.jpg"></img>
+                  <figcaption>Jeff</figcaption>
+                </figure>
+                <figure>
+                  <img src="http://res.cloudinary.com/dxfjyn0nq/image/upload/v1512346149/person4_eb7zgd.jpg"></img>
+                  <figcaption>Anna</figcaption>
+                </figure>
+              </div>
+              <p className="testimonial-subtitle">in San Francisco, California</p>
+              <div className="border"></div>
+              <p className="testimonial-text">
+                Anna is amazing! I loved spending time with her, and her openness and caring nature was so genuine and refreshing. When my suitcase didn't arrive until my second day, she was kind enough to loan me the comfiest pajamas to wear to sleep that first night, thank you again! A highlight of my trip includes celebrating her birthday with a dance party in the living room. Hope to see you again soon!
+              </p>
             </div>
             <div className="splash3-left">
               <h2>Meet New People</h2>
@@ -123,5 +128,9 @@ class Home extends React.Component {
   }
 }
 
+Home.proptypes = {
+  currentUser: PropTypes.object,
+  guestLogin: PropTypes.func
+}
 
 export default Home;
--- frontend/components/Pages/home_container.jsx
+++ frontend/components/Pages/home_container.jsx
@@ -1,11 +1,16 @@
 import Home from './home';
 import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
 
 const mapStateToProps = ({ session }) => ({
   currentUser: session.currentUser
 });
 
+const mapDispatchToProps = dispatch => ({
+  guestLogin: () => dispatch(login({email: "steph@gmail.com", password: "password1" }))
+})
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps,
 )(Home);
